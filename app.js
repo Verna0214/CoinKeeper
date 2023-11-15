bbd53936efcb4f7d7289c941f6eeb100681b7c55
Verna <yuhsichen0214@gmail.com>
2023-11-15 22:34:06 +0800

    feat: set view engine of handlebars

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+// include related modules
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const express = require('express')
+const handlebars = require('express-handlebars')
 
+const routes = require('./routes')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
-app.get('/', (req, res) => {
-    res.send('Hello, world.')
-})
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(routes)
 
 app.listen(port, () => {
     console.info(`App is running on http://localhost:${port}`)
