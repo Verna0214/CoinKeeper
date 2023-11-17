2459e6ba8e9880ec543486ce13af8ed2a49440f7
Verna <yuhsichen0214@gmail.com>
2023-11-17 09:38:46 +0800

    feat: add signUp feature

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,32 @@
+const bcrypt = require('bcryptjs')
+const Users = require('../models/user')
+
 const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
+  },
+  signUp: async (req, res) => {
+    try {
+      const { name, email, password, passwordConfirm } = req.body
+      if (password !== passwordConfirm) {
+        console.info('Passwords do not match!')
+        return res.redirect('/signup')
+      }
+      const user = await Users.findOne({ email })
+      if (user) {
+        console.info('User had an account already!')
+        return res.redirect('/signin')
+      }
+
+      await Users.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, 10)
+      })
+      res.redirect('/signin')
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
 
