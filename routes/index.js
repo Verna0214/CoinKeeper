59b51ab348ed67b80814837f63baf733433d47ec
Verna <yuhsichen0214@gmail.com>
2023-11-17 09:25:23 +0800

    feat: add signUp page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const userController = require('../controllers/user-controller')
+
+router.get('/signup', userController.signUpPage)
 router.get('/', (req, res) => {
   res.render('index')
 })
